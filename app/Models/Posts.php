50869e6f89eed2a743188696cbf8b99f0a238fb8
Mher2019 <balayan.mher88@gmail.com>
2021-07-07 11:06:23 +0400

    front for admin

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Posts extends Model
 {
     use HasFactory;
-    public $timestamps = false;
+    
     protected $table = 'posts';
     protected $fillable = [
         'image',
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,22 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Admin</title>
-</head>
-<body>
-@if($errors->any())
-    <div class="allert allert-warning ">
-    <ul>
-    @foreach($errors->all() as $error)
-    <li>{{$error}}</li>
-    @endforeach
-    </ul>
-    </div>
-    @endif
+@extends('leyout')
+@section('title')
+Admin
+@endsection
+@section('main_content')
+<div>
+<h1>Adding Posts</h1>
     <form method="post" action="admin/home_admin">
     @csrf
     <input type="text" name="text" id="text" placeholder="image url" class="form-control"><br>
@@ -24,5 +12,5 @@
     <textarea name="desc" id="desc" placeholder="desc" class="form-control"></textarea><br>
     <button type="submit" class="btn  btn-success">SEND</button>
     </form>
-</body>
-</html>
+    </div>
+@endsection
